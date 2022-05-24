22633f00334247e00b933b3b83ae3298a60fb880
Arthur Denner <arthurdenner7@gmail.com>
2022-05-24 21:36:55 +0200

    fix: change CORS configuration for api routes

--- pages/api/verify-token.ts
+++ pages/api/verify-token.ts
@@ -5,18 +5,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ invalidtoken: boolean; expired: boolean }>
 ) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
   const { token } = req.query;
   console.log(token);
   try {

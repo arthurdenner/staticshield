3f95d554c72fffd428fee5de2f9d60dcb9a1480d
Arthur Denner <arthurdenner7@gmail.com>
2022-05-24 22:53:19 +0200

    feat: simplify logic in script.js

--- packages/script.js
+++ packages/script.js
@@ -11,42 +11,41 @@ import BASE_URL from '../lib/baseUrl';
   const siteId = staticshieldsScript.getAttribute('data-site-id');
   const token = window.localStorage.getItem('staticshield-token');
 
-  if (token == null || undefined || !token) {
+  if (!token) {
     window.location.replace(
       `${BASE_URL}/p/${siteId}/?&redirecturl=${window.location.href
         .split('?')[0]
         .toString()}`
     );
+    return;
   }
 
-  if (token) {
-    const res = await fetch(
-      `${BASE_URL}/api/verify-token/?token=` + decodeURIComponent(token)
+  const res = await fetch(
+    `${BASE_URL}/api/verify-token/?token=` + decodeURIComponent(token)
+  );
+  const data = await res.json();
+  console.log(data);
+  if (data.expired == true) {
+    window.location.replace(
+      `${BASE_URL}/p/${siteId}/?expired=1&redirecturl=${window.location.href
+        .split('?')[0]
+        .toString()}`
     );
-    const data = await res.json();
-    console.log(data);
-    if (data.expired == true) {
-      window.location.replace(
-        `${BASE_URL}/p/${siteId}/?expired=1&redirecturl=${window.location.href
-          .split('?')[0]
-          .toString()}`
-      );
-    }
+  }
 
-    if (data.invalidtoken == true) {
-      window.location.replace(
-        `${BASE_URL}/p/${siteId}/?invalidtoken=1&&redirecturl=${window.location.href
-          .split('?')[0]
-          .toString()}`
-      );
-    }
-    if (data.invalidtoken == false && data.expired == false) {
-      window.staticshieldToken = localStorage.getItem('staticshield-token');
-      let asd = document
-        .querySelector('.staticshield-div')
-        .classList.toggle('staticshield-div');
-      console.log(asd);
-    }
+  if (data.invalidtoken == true) {
+    window.location.replace(
+      `${BASE_URL}/p/${siteId}/?invalidtoken=1&&redirecturl=${window.location.href
+        .split('?')[0]
+        .toString()}`
+    );
+  }
+  if (data.invalidtoken == false && data.expired == false) {
+    window.staticshieldToken = localStorage.getItem('staticshield-token');
+    let asd = document
+      .querySelector('.staticshield-div')
+      .classList.toggle('staticshield-div');
+    console.log(asd);
   }
 })();
 
